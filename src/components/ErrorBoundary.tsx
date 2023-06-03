79c0f7978aaefe56e48aaf28a29b67ea8127b11e
kosukekashiwa <hanjuku.kabocha49@gmail.com>
2023-06-03 15:03:02 +0900

    ErrorBoundary

---
+++ src/components/ErrorBoundary.tsx
@@ -0,0 +1,27 @@
+'use client'
+import React, { ReactNode } from "react";
+
+type ErrorBoundaryProps = { fallback: ReactNode, children: ReactNode };
+
+class ErrorBoundary extends React.Component<ErrorBoundaryProps, { hasError: boolean }> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = {
+      hasError: false,
+    };
+  }
+
+  static getDerivedStateFromError(): { hasError: boolean } {
+    return { hasError: true };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <>{this.props.fallback}</>;
+    }
+    
+    return <>{this.props.children}</>;
+  }
+}
+
+export default ErrorBoundary;
