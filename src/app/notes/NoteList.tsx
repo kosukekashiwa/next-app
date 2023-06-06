c681e3d2546eb1fbfec22f0f99be1f67cf10154e
kosukekashiwa <hanjuku.kabocha49@gmail.com>
2023-06-06 19:15:32 +0900

    feature ノート詳細画面の追加

--- src/app/notes/NoteList.tsx
+++ src/app/notes/NoteList.tsx
@@ -39,8 +39,7 @@ const NoteItem: React.FC<NoteItemProps> = ({ item }) => {
           </svg>
         </span>
       </Link>
-      { /* ノート詳細ページは未実装のため一覧ページに遷移 */ }
-      <Link href={`/notes`} prefetch={false}>
+      <Link href={`/notes/${item.id}`} prefetch={false}>
         <h3 className="text-pink-500 hover:text-pink-700 text-lg md:text-xl font-semibold mb-3 break-all underline underline-offset-2">{item.title}</h3>
       </Link>
       <p className="text-gray-500 break-all">{item.body}</p>
---
+++ src/app/notes/[id]/getNote.ts
@@ -0,0 +1,10 @@
+import { apiUrl } from "@/src/constants/api";
+import "server-only";
+import { zNote } from "../type";
+
+export const getNote = async (id: string) => {
+  const res = await fetch(`${apiUrl}/notes/${id}`, { cache: 'no-store' });
+  const data = await res.json();
+  const note = zNote.parse(data);
+  return note;
+};
