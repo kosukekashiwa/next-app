9b375269fab8d94c2599b41880a4c221ffde5a41
kosukekashiwa <hanjuku.kabocha49@gmail.com>
2023-06-15 08:15:17 +0900

    Stack

---
+++ src/components/layouts/Stack.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+
+type StackProps = {
+  children: React.ReactNode;
+  direction?: keyof typeof directionStyle;
+  alignItems?: keyof typeof alignItemsStyle;
+  justifyContent?: keyof typeof justifyContentStyle;
+  space?: keyof typeof spaceStyle;
+  // ToDo: divider props and Divider Component.
+  // divider: React.ReactNode;
+};
+
+const Stack: React.FC<StackProps> = ({
+  children,
+  direction = 'row',
+  alignItems = 'center',
+  justifyContent = 'center',
+  space = '8',
+}) => {
+  const childrenArray = React.Children.toArray(children).filter(Boolean);
+
+  return (
+    <div
+      className={`flex ${directionStyle[direction]} ${alignItemsStyle[alignItems]} ${justifyContentStyle[justifyContent]} ${spaceStyle[space]}`}
+    >
+      {childrenArray.map((item) => {
+        return item;
+      })}
+    </div>
+  );
+};
+
+const directionStyle = {
+  row: 'flex-row',
+  rowReverse: 'flex-row-reverse',
+  column: 'flex-col',
+  columnReverse: 'flex-col-reverse',
+};
+
+const alignItemsStyle = {
+  flexStart: 'items-start',
+  center: 'items-center',
+  flexEnd: 'items-end',
+  stretch: 'stretch',
+  baseline: 'baseline',
+};
+
+const justifyContentStyle = {
+  flexStart: 'justify-start',
+  center: 'justify-center',
+  flexEnd: 'justify-end',
+  spaceBetween: 'justify-between',
+};
+const spaceStyle = {
+  '0': 'gap-0',
+  '1': 'gap-[1px]',
+  '2': 'gap-[2px]',
+  '4': 'gap-[4px]',
+  '8': 'gap-[8px]',
+  '12': 'gap-[12px]',
+  '16': 'gap-[16px]',
+  '24': 'gap-[24px]',
+  '32': 'gap-[32px]',
+  '48': 'gap-[48px]',
+};
+
+export default Stack;
